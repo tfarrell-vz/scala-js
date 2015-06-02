2f91254977673c0450579beba8239abd0d178c7d
tfarrell-vz <farrell@abiresearch.com>
2015-06-02 14:39:25 -0400

    Hello World! in scala.js

---
+++ src/main/scala/webapp/TutorialApp.scala
@@ -0,0 +1,11 @@
+package tutorial.webapp
+
+import scala.scalajs.js.JSApp
+
+object TutorialApp extends JSApp {
+  def main(): Unit = {
+    println("Hello world!")
+  }
+}
+
+// vim: set ts=2 sw=2 et sts=2:
